658790e29178ac10f287b816d6c327272099834b
woddwill-cyber <woddwill@gmail.com>
2026-07-01 17:30:36 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'kelime-kutusu-v1';
+const ASSETS = [
+  'Diluygulaması.html',
+  'manifest.json',
+  'icon.png'
+];
+
+// Uygulama yüklenirken dosyaları hafızaya al
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// İnternet olmasa bile hafızadaki dosyaları kullan
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
